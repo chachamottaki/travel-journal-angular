26edd7d909e4985d89d3fd540c58c95828b7846f
LAPTOP-K8A78D85\marsh <marshymellowness@gmail.com>
2023-06-04 18:17:10 +0200

    created header component

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  template: `
+  <div class='header'>
+    <div class='title'> Travel Confessions </div>
+  </div>
+  `,
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+
+}
