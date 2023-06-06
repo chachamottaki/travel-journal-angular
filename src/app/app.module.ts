99ad2f0b177ce089f440f95a5f3bc732403bf7ad
LAPTOP-K8A78D85\marsh <marshymellowness@gmail.com>
2023-06-06 13:10:08 +0200

    import formsmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -31,6 +32,7 @@ import { EntryFormComponent } from './entry-form/entry-form.component';
     MatInputModule,
     MatButtonModule,
     MatIconModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
