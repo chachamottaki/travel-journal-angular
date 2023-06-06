3990ffafe5867ebe511f86fcff6b4a6908f4d4a6
LAPTOP-K8A78D85\marsh <marshymellowness@gmail.com>
2023-06-06 20:43:24 +0200

    changed login layout

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service'
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [UserService]
 })
-export class LoginComponent {
-  submit(){
-    console.log("form submitted :)")
+export class LoginComponent implements OnInit {
+  // formGroup:FormGroup;
+
+  //form function
+  // submit(){
+  //   console.log("form submitted :)")
+  // }
+
+  getLoginFromService(){
+    // this.email, this.password = this.userservice.login(email,password)
+  }
+
+  constructor(private userservice: UserService){}
+
+  ngOnInit(): void {
+    
   }
 }
