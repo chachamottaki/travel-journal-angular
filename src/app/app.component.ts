a48b08575dd47f5884712396f2567ff5494b8b52
LAPTOP-K8A78D85\marsh <marshymellowness@gmail.com>
2023-06-06 16:17:39 +0200

    added signup component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'travel-journal';
+  private baseUrl = 'http://localhost:8000/login';
+
+  constructor(private http: HttpClient) { }
+  
+  ngOnInit(): void {
+    }
+    
 }
